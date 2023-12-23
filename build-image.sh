ecec9acc91f38d2d1a9f71999734fc5d9096dbc2
Aleksey Kurmanov <aleksey.kurmanov@gmail.com>
2023-12-23 01:50:52 -0500

    play with vars #01

--- build-image.sh
+++ build-image.sh
@@ -2,7 +2,7 @@
 
 export DOCKERFILE_NAME="Dockerfile-${DEPLOYMENT_NAME:="<<unknown-deployment>>"}"
 export DOCKER_IMAGE_NAME="dpp-$DEPLOYMENT_NAME"
-export DOCKER_USERNAME=${DOCKER_USER_NAME:="<<unknown-docker-username>>"}
+export DOCKER_USERNAME=${DOCKER_USERNAME:="<<unknown-docker-username>>"}
 export DOCKER_PASSWORD=${DOCKER_PASSWORD:="<<unknown-docker-password>>"}
 
 echo "Going to build and push the docker-mage with following settings:"
@@ -10,7 +10,7 @@ echo "DEPLOYMENT_NAME   = '$DEPLOYMENT_NAME'"
 echo "DOCKERFILE_NAME   = '$DOCKERFILE_NAME'"
 echo "DOCKER_IMAGE_NAME = '$DOCKER_IMAGE_NAME'"
 echo "DOCKER_USERNAME = '$DOCKER_USERNAME'"
-echo "DOCKER_PASSWORD has length " $#DOCKER_PASSWORD
+echo "DOCKER_PASSWORD has length " ${#DOCKER_PASSWORD}
 
 env | sort > ./env.txt
 
