a5bed159555dc6d6a0b4d9f13ad76ba97802c171
Aleksey Kurmanov <aleksey.kurmanov@gmail.com>
2023-12-26 08:06:27 -0500

    remove debugging info from 'pre-build-hello-sh.sh'

--- pre-build-hello-sh.sh
+++ pre-build-hello-sh.sh
@@ -3,17 +3,4 @@
 #   Pre-Build script for deployment-type 'hello-sh', invoked from 'build-image.sh'
 # ------------------------------------------------------------------------------------
 env | sort > ./env.txt
-echo "attempt to use 'jq' utility:"
-echo '{"foo": 0, "arr": [1.2, "la-la-la", true]}' | jq
-echo "attempt to dump docker system-info:"
-echo "-----------------------------------"
-docker system info
-echo "-------- and as 'json': -----------"
-docker system info --format 'json'
-echo "-------- as {{json .}}: -----------"
-docker system info --format '{{json .}}'
-echo "-------- without system: ----------"
-docker info --format '{{json .}}'
-echo "-----------------------------------"
-echo "saving docker system-info into './env-docker-system-info.json' ..."
-docker system info --format json | jq > ./env-docker-system-info.json
+docker system info --format '{{json .}}' | jq > ./env-docker-system-info.json
