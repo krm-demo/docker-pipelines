c54a728fc6902e2e42374ff2a9ab7159e9bf24bf
Aleksey Kurmanov <aleksey.kurmanov@gmail.com>
2023-12-26 07:07:14 -0500

    invetigate problems with 'docker system info'

--- pre-build-hello-sh.sh
+++ pre-build-hello-sh.sh
@@ -5,4 +5,9 @@
 env | sort > ./env.txt
 echo "attempt to use 'jq' utility:"
 echo '{"foo": 0, "arr": [1.2, "la-la-la", true]}' | jq
+echo "attempt to dump docker system-info:"
+echo "-----------------------------------"
+docker system info --format json
+echo "-----------------------------------"
+echo "saving docker system-info into './env-docker-system-info.json' ..."
 docker system info --format json | jq > ./env-docker-system-info.json
