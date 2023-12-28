4159c5b6bb9ebceb7db5732c0a39847c3a3b1d7b
Aleksey Kurmanov <aleksey.kurmanov@gmail.com>
2023-12-28 13:36:03 -0500

    Remove debug options from sha-bang

--- src/main/java/org/krmdemo/app/hello/HelloApp.java
+++ src/main/java/org/krmdemo/app/hello/HelloApp.java
@@ -1,5 +1,6 @@
 package org.krmdemo.app.hello;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -18,11 +19,13 @@ import static org.krmdemo.restinfo.util.StreamUtils.toSortedMap;
 @SpringBootApplication
 public class HelloApp implements CommandLineRunner {
 
+    @Value("${spring.application.name}")
+    private String applicationName;
+
     @Override
     public void run(String... args) {
-        System.out.printf("Hello from '%s' at %s\n", this.getClass().getSimpleName(), ZonedDateTime.now());
+        System.out.printf("Hello from Spring-Boot application '%s' at %s\n", applicationName, ZonedDateTime.now());
         System.out.println("=================================================");
-
         System.out.println("runtime-environment on start-up:");
         System.out.println("-------------------------------------------------");
         dumpCmd("uname", "-s"); // --kernel-name
@@ -35,7 +38,6 @@ public class HelloApp implements CommandLineRunner {
         dumpCmd("uname", "-a"); // --all
         dumpProps(new TreeMap<>(System.getenv()));
         System.out.println("=================================================");
-
         System.out.println("java system-properties on start-up:");
         System.out.println("-------------------------------------------------");
         dumpProps(toSortedMap(System.getProperties()));
