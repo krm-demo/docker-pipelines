8f94bf48cfcffca4b6c8107c558dfe11b453c23a
Aleksey Kurmanov <aleksey.kurmanov@gmail.com>
2024-01-08 01:56:46 -0500

    Invoke 'verify' phase during maven-build

--- pre-build-hello-maven.sh
+++ pre-build-hello-maven.sh
@@ -2,4 +2,4 @@
 # ------------------------------------------------------------------------------------
 #   Pre-Build script for deployment-type 'hello-maven', invoked from 'build-image.sh'
 # ------------------------------------------------------------------------------------
-$DOCKER_IMAGE_BASE_DIR/mvnw clean package
+$DOCKER_IMAGE_BASE_DIR/mvnw clean verify
